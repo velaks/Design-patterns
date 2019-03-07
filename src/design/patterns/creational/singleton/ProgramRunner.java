2bd48618c0872689be2f50cca9d1a2c73cb6d094
Efim Moskalev <efim.moskalev1911@gmail.com>
2019-03-07 23:54:33 +0400

    singleton

---
+++ src/design/patterns/creational/singleton/ProgramRunner.java
@@ -0,0 +1,12 @@
+package design.patterns.creational.singleton;
+
+public class ProgramRunner {
+	public static void main(String[] args) {
+		ProgramLogger.getProgramLogger().addLogInfo("First log");
+		ProgramLogger.getProgramLogger().addLogInfo("Second log");
+		ProgramLogger.getProgramLogger().addLogInfo("Third log");
+		
+		ProgramLogger.getProgramLogger().showLogFile();
+	}
+
+}
